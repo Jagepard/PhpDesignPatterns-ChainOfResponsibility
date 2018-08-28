705e574ac70e61b229eafdf2b7dcd95edb2772f4
Jagepard <dankorot@gmail.com>
2018-08-28 22:48:42 +0300

    Simplified

--- src/AbstractHandler.php
+++ src/AbstractHandler.php
@@ -16,32 +16,5 @@ namespace Behavioral\ChainOfResponsibility;
 abstract class AbstractHandler
 {
 
-    /**
-     * AbstractHandler constructor.
-     * @param int   $count
-     * @param array $chain
-     */
-    public function __invoke(int $count, array $chain)
-    {
-        array_shift($chain);
-        $this->request();
-        $this->next($count - 1, $chain);
-    }
-
-    /**
-     * @param int   $count
-     * @param array $chain
-     */
-    protected function next(int $count, array $chain): void
-    {
-        if (!($count and count($chain))) {
-            printf(PHP_EOL);
-            return;
-        }
-
-        $nextChain = new $chain[0] ?? new $chain;
-        $nextChain($count, $chain);
-    }
-
     abstract public function request();
 }
