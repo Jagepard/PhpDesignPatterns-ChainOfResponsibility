fc26e02d5ea2ec42b361a170a4c62f3f04cadb77
Jagepard <dankorot@gmail.com>
2018-08-29 10:19:23 +0300

    Simplify

--- src/AbstractHandler.php
+++ src/AbstractHandler.php
@@ -18,23 +18,6 @@ abstract class AbstractHandler
 
     /**
      * @param string $event
-     * @param        $chain
      */
-    abstract public function request(string $event, $chain): void;
-
-    /**
-     * @param string $event
-     * @param        $chain
-     */
-    protected function next(string $event, $chain)
-    {
-        !is_array($chain) ?: array_shift($chain);
-
-        if (!count($chain)) {
-            return;
-        }
-
-        $handler = is_array($chain) ? new $chain[0]() : new $chain();
-        $handler->request($event, $chain);
-    }
+    abstract public function request(string $event): void;
 }
