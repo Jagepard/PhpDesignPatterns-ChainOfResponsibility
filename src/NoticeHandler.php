0adaa9f6a1a2123fbb68ab771a2b2e89bbd3b2bd
Jagepard <dankorot@gmail.com>
2019-04-05 20:09:20 +0300

    Update styling

--- src/NoticeHandler.php
+++ src/NoticeHandler.php
@@ -3,16 +3,12 @@
 declare(strict_types=1);
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Korotkov Danila <dankorot@gmail.com>
+ * @license https://mit-license.org/ MIT
  */
 
 namespace Behavioral\ChainOfResponsibility;
 
-/**
- * Class NoticeHandler
- * @package Behavioral\ChainOfResponsibility
- */
 class NoticeHandler implements HandlerInterface
 {
     use NextTrait;
--- src/WarningHandler.php
+++ src/WarningHandler.php
@@ -3,16 +3,12 @@
 declare(strict_types=1);
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Korotkov Danila <dankorot@gmail.com>
+ * @license https://mit-license.org/ MIT
  */
 
 namespace Behavioral\ChainOfResponsibility;
 
-/**
- * Class WarningHandler
- * @package Behavioral\ChainOfResponsibility
- */
 class WarningHandler implements HandlerInterface
 {
     use NextTrait;
