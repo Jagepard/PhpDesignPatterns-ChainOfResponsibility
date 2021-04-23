0790f69c2b63804676db42962d0e80ea3fd2034b
Jagepard <dankorot@gmail.com>
2021-04-23 16:23:34 +0300

    add russian

--- src/AbstractHandler.php
+++ src/AbstractHandler.php
@@ -15,18 +15,25 @@ abstract class AbstractHandler implements ChainInterface
     /**
      * If the condition matches, code is executed;
      * if not, then it is passed along the chain to the next handler
+     * -------------------------------------------------------------
+     * Если условие совпадает, код выполняется;
+     * Если нет, то он передается по цепочке следующему обработчику
+     *
+     * @param string $request
+     * @param bool $allInChain
      */
     public function execute(string $request, bool $allInChain = false): void
     {
         if ($allInChain) {
             printf("%s %s\n", get_called_class(), "has handle a request");
 
-            if ($request === $this->getName()) {
+            if ($request === $this->name) {
                 return;
             }
         } else {
             // In case of compliance, the code is executed
-            if ($request === $this->getName()) {
+            // В случае соответствия код выполняется
+            if ($request === $this->name) {
                 printf("%s %s\n", get_called_class(), "has handle a request");
                 return;
             }
@@ -37,20 +44,21 @@ abstract class AbstractHandler implements ChainInterface
         }
 
         // Passed to the next handler
+        // Передано следующему обработчику
         $this->nextHandler->execute($request, $allInChain);
     }
 
     /**
      * Adds the next handler to the chain
+     * ----------------------------------
+     * Добавляет следующий обработчик в цепочку
+     *
+     * @param AbstractHandler $handler
+     * @return AbstractHandler
      */
     public function setNext(AbstractHandler $handler): AbstractHandler
     {
         $this->nextHandler = $handler;
         return $handler;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
     }
 }
